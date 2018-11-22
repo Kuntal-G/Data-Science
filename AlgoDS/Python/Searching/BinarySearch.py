67ff12c79cc9d869ee61d161b575c8c3abee8424
Ganguly <gangulykuntal1986@gmail.com>
2018-11-22 21:27:53 +0000

    Linear & Binary Search

---
+++ AlgoDS/Python/Searching/BinarySearch.py
@@ -0,0 +1,28 @@
+# Binary serach implementation
+def binarySearch(arr,k):
+
+    low=0
+    high=len(arr)-1
+    found= False
+
+    while low<=high and not found:
+        mid = (low + high) // 2
+
+        if k == arr[mid]:
+            found=True
+            print('Element {} found at position {}'.format(k,mid))
+
+        else:
+            if k>arr[mid]:
+                low=mid +1
+            else:
+                high=mid-1
+    return found
+
+arr=[2,4,6,8.10]
+print(binarySearch(arr,6))
+
+
+
+
+
