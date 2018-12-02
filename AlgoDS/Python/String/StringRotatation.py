5ae0b93ff104c60713de018026c671f0e0d36076
Ganguly <gangulykuntal1986@gmail.com>
2018-12-02 22:51:59 +0000

    Updated String/Array Rotation using modulo logic

--- AlgoDS/Python/String/StringRotatation.py
+++ AlgoDS/Python/String/StringRotatation.py
@@ -21,3 +21,47 @@ str1=[1,2,3,4,5]
 print(stringRotation(str1,4,'left'))
 
 
+#String/Array rotation using modulo logic
+def leftRotate(arr,k):
+
+    n=len(arr)
+    mod = k % n
+    print('mod',mod)
+    s = []
+
+
+    for i in range(n):
+        s.append(arr[(mod + i) % n])
+
+    return s
+
+#left rotate variant
+def leftRotate2(arr,k):
+
+    n=len(arr)
+    s = []
+
+    for i in range(k,k+n):
+        s.append(arr[i % n])
+
+    return s
+
+
+arr = [ 1, 3, 5, 7, 9 ]
+print(leftRotate(arr,3))
+print(leftRotate2(arr,3))
+
+#Right rotate with modulo
+def right_rotate(arr,k):
+    s=[]
+    n=len(arr)
+    for i in range(n):
+        s.insert((i+k)%n,arr[i])
+
+    return s
+print(right_rotate(arr,3))
+
+
+
+
+
